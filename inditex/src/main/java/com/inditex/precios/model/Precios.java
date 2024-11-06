67928cdb4a5a34a18066c2417e9efadc9549ed7e
aitorlzm <aitorlopezm_md@hotmail.com>
2024-11-06 15:21:30 +0100

    Update Precios.java

--- inditex/src/main/java/com/inditex/precios/model/Precios.java
+++ inditex/src/main/java/com/inditex/precios/model/Precios.java
@@ -1,23 +1,102 @@
 package com.inditex.precios.model;
 
+import java.math.BigInteger;
 import java.time.LocalDateTime;
 
 import org.springframework.data.annotation.Id;
 
 import jakarta.persistence.Entity;
 
-@Entity	
+@Entity
 public class Precios {
-	
+
 	@Id
-	private int brandId;
+	private Long brandId;
 	private LocalDateTime startDate;
-	private LocalDateTime startEnd;
-	private int priceList;
-	private int productId;
-	private int priority;
-	private String curr;
-	
-	
+	private LocalDateTime endDate;
+	private Integer priceList;
+	private BigInteger price;
+	private Integer productId;
+	private Integer priority;
+	private String currency;
+
+	public Precios(Long brandId, LocalDateTime startDate, LocalDateTime endDate, Integer priceList, BigInteger price,
+			Integer productId, Integer priority, String currency) {
+		this.brandId = brandId;
+		this.startDate = startDate;
+		this.endDate = endDate;
+		this.priceList = priceList;
+		this.price = price;
+		this.productId = productId;
+		this.priority = priority;
+		this.currency = currency;
+	}
+
+	public Precios() {
+	}
+
+	public Long getBrandId() {
+		return brandId;
+	}
+
+	public void setBrandId(Long brandId) {
+		this.brandId = brandId;
+	}
+
+	public LocalDateTime getStartDate() {
+		return startDate;
+	}
+
+	public void setStartDate(LocalDateTime startDate) {
+		this.startDate = startDate;
+	}
+
+	public LocalDateTime getEndDate() {
+		return endDate;
+	}
+
+	public void setEndDate(LocalDateTime endDate) {
+		this.endDate = endDate;
+	}
+
+	public Integer getPriceList() {
+		return priceList;
+	}
+
+	public void setPriceList(Integer priceList) {
+		this.priceList = priceList;
+	}
+
+	public BigInteger getPrice() {
+		return price;
+	}
+
+	public void setPrice(BigInteger price) {
+		this.price = price;
+	}
+
+	public Integer getProductId() {
+		return productId;
+	}
+
+	public void setProductId(Integer productId) {
+		this.productId = productId;
+	}
+
+	public Integer getPriority() {
+		return priority;
+	}
+
+	public void setPriority(Integer priority) {
+		this.priority = priority;
+	}
+
+	public String getCurrency() {
+		return currency;
+	}
+
+	public void setCurrency(String currency) {
+		this.currency = currency;
+	}
 
 }
