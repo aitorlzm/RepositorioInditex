3e0b4c6ef4f4ee88160390da08ce6c4a7f08bae0
aitorlzm <aitorlopezm_md@hotmail.com>
2024-11-07 13:21:49 +0100

    Creacion tests endpoint

--- inditex/src/test/java/com/inditex/precios/PreciosControllerTest.java
+++ inditex/src/test/java/com/inditex/precios/PreciosControllerTest.java
@@ -19,9 +19,9 @@ class PreciosControllerTest  {
 	
 	private final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd-HH.mm.ss");
 	
-	// Test 1
+    // Test 1: petición a las 10:00 del día 14 del producto 35455 para la brand 1
 	@Test
-	public void test1 () {
+	public void test1 () throws Exception {
 		mockMvc.perform(get("/api/precios/consultar")
 				.param("fecha", "2020-06-14-10.00.00")
                 .param("productoId", "35455")
@@ -32,8 +32,57 @@ class PreciosControllerTest  {
                 .andExpect(jsonPath("$.price").value(35.50));
 	}
 	
+    // Test 2: petición a las 16:00 del día 14 del producto 35455 para la brand 1
 	@Test
-	void contextLoads() {
-	}
+    public void test2() throws Exception {
+        mockMvc.perform(get("/api/precios/consultar")
+                .param("fecha", "2020-06-14-16.00.00")
+                .param("productoId", "35455")
+                .param("marcaId", "1"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.productId").value(35455))
+                .andExpect(jsonPath("$.brandId").value(1))
+                .andExpect(jsonPath("$.price").value(25.45));
+    }
+
+    // Test 3: petición a las 21:00 del día 14 del producto 35455 para la brand 1
+    @Test
+    public void test3() throws Exception {
+        mockMvc.perform(get("/api/precios/consultar")
+                .param("fecha", "2020-06-14-21.00.00")
+                .param("productoId", "35455")
+                .param("marcaId", "1"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.productId").value(35455))
+                .andExpect(jsonPath("$.brandId").value(1))
+                .andExpect(jsonPath("$.price").value(35.50));
+    }
+
+    // Test 4: petición a las 10:00 del día 15 del producto 35455 para la brand 1
+    @Test
+    public void test4() throws Exception {
+        mockMvc.perform(get("/api/precios/consultar")
+                .param("fecha", "2020-06-15-10.00.00")
+                .param("productoId", "35455")
+                .param("marcaId", "1"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.productId").value(35455))
+                .andExpect(jsonPath("$.brandId").value(1))
+                .andExpect(jsonPath("$.price").value(30.50));
+    }
+
+    // Test 5: petición a las 21:00 del día 16 del producto 35455 para la brand 1
+    @Test
+    public void test5() throws Exception {
+        mockMvc.perform(get("/api/precios/consultar")
+                .param("fecha", "2020-06-16-21.00.00")
+                .param("productoId", "35455")
+                .param("marcaId", "1"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.productId").value(35455))
+                .andExpect(jsonPath("$.brandId").value(1))
+                .andExpect(jsonPath("$.price").value(38.95));
+    }
+	
 
 }
